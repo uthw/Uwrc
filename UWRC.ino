bc5dedd0bf029896bae7135d3e611f48e0f2db5c
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-12-18 16:14:25 -0500

    Add a second joystick
    We don't have the motor yet

--- UWRC.ino
+++ UWRC.ino
@@ -1,14 +1,30 @@
-#define XP A0
-#define YP A1
+// Joysitck 1
+#define XP1MOTER1 11 // 6
+#define XP2MOTER1 10 // 3
 
-#define XP1 11 // 6
-#define XP2 10 // 3
+#define XPJOY1 A0 
+#define YPJOY1 A1
 
-#define XMAX 1024
-#define XMIN 1
+#define XMAX1 1024
+#define XMIN1 1
 
-#define YMAX 1024
-#define YMIN 1003
+#define YMAX1 1024
+#define YMIN1 1003
+// End Joysitck 1
+
+// Joysitck 2 NOT READY
+#define XP1MOTER2 11 // 6
+#define XP2MOTER2 10 // 3
+
+#define XPJOY2 A2 
+#define YPJOY2 A3
+
+#define XMAX2 1024
+#define XMIN2 1
+
+#define YMAX2 1024
+#define YMIN2 1003
+// End Joysitck 2
 
 #define DEADMAX 100
 #define DEADMIN -100
@@ -16,34 +32,64 @@
 void setup() {
  Serial.begin(9600);
 
- pinMode(XP1, OUTPUT);
- pinMode(XP2, OUTPUT);
+ pinMode(XP1MOTER1, OUTPUT);
+ pinMode(XP2MOTER1, OUTPUT);
+
+ pinMode(XP1MOTER2, OUTPUT);
+ pinMode(XP2MOTER2, OUTPUT);
 }
 
 void loop() {
 
-  double xpower = map(analogRead(XP), XMAX, XMIN, 255, -255); 
-  // kill yourself
-  double ypower = (analogRead(YP) - YMIN) * (255 - -255) / (YMAX - YMIN) + -255;
-  
+  // Joysitck 1
+  double xpower1 = map(analogRead(XPJOY1), XMAX1, XMIN1, 255, -255); 
+  // NO THANK YOU double ypower1 = map(analogRead(YPJOY1), YMAX1, YMIN1, 255, -255); 
+  // End Joystick 1
 
-  // x joystic
-  if (xpower > DEADMAX)
+  // Joysitck 1
+  double xpower2 = map(analogRead(XPJOY2), XMAX2, XMIN2, 255, -255); 
+  // NO THANK YOU double ypower1 = map(analogRead(YPJOY1), YMAX1, YMIN1, 255, -255); 
+  // End Joystick 1
+
+
+  // x joystick 1
+  if (xpower1 > DEADMAX)
   {
     Serial.println("A");
-    analogWrite(XP1, xpower);
-    analogWrite(XP2, LOW);
+    analogWrite(XP1MOTER1, xpower1);
+    analogWrite(XP2MOTER1, LOW);
   }
-  else if (xpower < DEADMIN)
+  else if (xpower1 < DEADMIN)
   {
     Serial.println("B");
-    analogWrite(XP2, xpower * -1);
-    analogWrite(XP1, LOW);
+    analogWrite(XP2MOTER1, xpower1 * -1);
+    analogWrite(XP1MOTER1, LOW);
   }
   else
   {
-    analogWrite(XP1, LOW);
-    analogWrite(XP2, LOW);
+    Serial.println("N");
+    analogWrite(XP1MOTER1 , LOW);
+    analogWrite(XP2MOTER1 , LOW);
+  }
+
+  // x joystick 2
+  if (xpower2 > DEADMAX)
+  {
+    Serial.println("A");
+    analogWrite(XP1MOTER2, xpower2);
+    analogWrite(XP2MOTER2, LOW);
+  }
+  else if (xpower2 < DEADMIN)
+  {
+    Serial.println("B");
+    analogWrite(XP2MOTER2, xpower2 * -1);
+    analogWrite(XP1MOTER2, LOW);
+  }
+  else
+  {
+    Serial.println("N");
+    analogWrite(XP1MOTER2 , LOW);
+    analogWrite(XP2MOTER2 , LOW);
   }
 }
   
