50a5e47e5e21dbd3f46855895bdc1095965d82a1
uthw <u.wood@icloud.com>
2024-01-29 11:52:09 -0500

    Actual refactor

--- UWRC.ino
+++ UWRC.ino
@@ -1,32 +1,21 @@
 //#include <LiquidCrystal.h>
 
-// Joysitck 1
-#define XP1MOTER1 6 // 6
-#define XP2MOTER1 5 // 3
+struct JoystickConfig_s {
+  int xPin1Motor;
+  int xPin2Motor;
+  int xPinJoy;
+  int yPinJoy;
+  int xMax;
+  int xMin;
+  int yMax;
+  int yMin;
+};
 
-#define XPJOY1 A0 
-#define YPJOY1 A1
+// Initialize default values
+struct JoystickConfig_s JoystickConfigDefault = {0, 0, 0, 0, 1024, 1, 1024, 1003};
 
-#define XMAX1 1024
-#define XMIN1 1
-
-#define YMAX1 1024
-#define YMIN1 1003
-// End Joysitck 1
-
-// Joysitck 2 NOT READY
-#define XP1MOTER2 11 // 6
-#define XP2MOTER2 10 // 3
-
-#define XPJOY2 A2 
-#define YPJOY2 A3
-
-#define XMAX2 1024
-#define XMIN2 1
-
-#define YMAX2 1024
-#define YMIN2 1003
-// End Joysitck 2
+struct JoystickConfig_s joy1 = JoystickConfigDefault;
+struct JoystickConfig_s joy2 = JoystickConfigDefault;
 
 #define DEADMAX 100
 #define DEADMIN -100
@@ -36,66 +25,61 @@ int tempPin = 0;
 //LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
- Serial.begin(9600);
+  Serial.begin(9600);
 
- pinMode(XP1MOTER1, OUTPUT);
- pinMode(XP2MOTER1, OUTPUT);
+  // define configuration for joystick 1:
+  joy1.xPin1Motor = 6;
+  joy1.xPin2Motor = 5;
+  joy1.xPinJoy = A0;
+  joy1.yPinJoy = A1;
 
- pinMode(XP1MOTER2, OUTPUT);
- pinMode(XP2MOTER2, OUTPUT);
+  // define configuration for joystick 2:
+  joy2.xPin1Motor = 11; // 6
+  joy2.xPin2Motor = 10; // 3
+  joy2.xPinJoy = A2;
+  joy2.yPinJoy = A3;
+
+  pinMode(joy1.xPin1Motor, OUTPUT);
+  pinMode(joy1.xPin2Motor, OUTPUT);
+
+  pinMode(joy2.xPin1Motor, OUTPUT);
+  pinMode(joy2.xPin2Motor, OUTPUT);
 }
 
 void loop() {
+  // Joystick 1 Power Range
+  double xpower1 = map(analogRead(joy1.xPinJoy), joy1.xMax, joy1.xMin, 255, -255);
 
-  // Joysitck 1
-  double xpower1 = map(analogRead(XPJOY1), XMAX1, XMIN1, 255, -255); 
-  // NO THANK YOU double ypower1 = map(analogRead(YPJOY1), YMAX1, YMIN1, 255, -255); 
-  // End Joystick 1
-
-  // Joysitck 1
-  double xpower2 = map(analogRead(XPJOY2), XMAX2, XMIN2, 255, -255); 
-  // NO THANK YOU double ypower1 = map(analogRead(YPJOY1), YMAX1, YMIN1, 255, -255); 
-  // End Joystick 1
-
+  // Joystick 2 Power Range
+  double xpower2 = map(analogRead(joy2.xPinJoy), joy2.xMax, joy2.xMin, 255, -255);
 
   // x joystick 1
-  if (xpower1 > DEADMAX)
-  {
+  if (xpower1 > DEADMAX) {
     Serial.println("A");
-    analogWrite(XP1MOTER1, xpower1);
-    analogWrite(XP2MOTER1, LOW);
-  }
-  else if (xpower1 < DEADMIN)
-  {
+    analogWrite(joy1.xPin1Motor, xpower1);
+    analogWrite(joy1.xPin2Motor, LOW);
+  } else if (xpower1 < DEADMIN) {
     Serial.println("B");
-    analogWrite(XP2MOTER1, xpower1 * -1);
-    analogWrite(XP1MOTER1, LOW);
-  }
-  else
-  {
+    analogWrite(joy1.xPin2Motor, xpower1 * -1);
+    analogWrite(joy1.xPin1Motor, LOW);
+  } else {
     Serial.println("N");
-    analogWrite(XP1MOTER1 , LOW);
-    analogWrite(XP2MOTER1 , LOW);
+    analogWrite(joy1.xPin1Motor, LOW);
+    analogWrite(joy1.xPin2Motor, LOW);
   }
 
   // x joystick 2
-  if (xpower2 > DEADMAX)
-  {
+  if (xpower2 > DEADMAX) {
     Serial.println("A");
-    analogWrite(XP1MOTER2, xpower2);
-    analogWrite(XP2MOTER2, LOW);
-  }
-  else if (xpower2 < DEADMIN)
-  {
+    analogWrite(joy2.xPin1Motor, xpower2);
+    analogWrite(joy2.xPin2Motor, LOW);
+  } else if (xpower2 < DEADMIN) {
     Serial.println("B");
-    analogWrite(XP2MOTER2, xpower2 * -1);
-    analogWrite(XP1MOTER2, LOW);
-  }
-  else
-  {
+    analogWrite(joy2.xPin2Motor, xpower2 * -1);
+    analogWrite(joy2.xPin1Motor, LOW);
+  } else {
     Serial.println("N");
-    analogWrite(XP1MOTER2 , LOW);
-    analogWrite(XP2MOTER2 , LOW);
+    analogWrite(joy2.xPin1Motor, LOW);
+    analogWrite(joy2.xPin2Motor, LOW);
   }
 }
-  
