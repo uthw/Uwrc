fc12775b330428d1ad264519c7d936a80b2b4f0c
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2024-02-01 11:13:50 -0500

    Add files via upload

--- Thermometer.ino
+++ Thermometer.ino
@@ -11,32 +11,29 @@ void setup()
 }
 void loop()
 {
-// / int tempReading = analogRead(tempPin);
+  int tempReading = analogRead(tempPin);
   // This is OK
-//  /double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-//  /tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
-//  /float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
-//  /float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-  /*  replaced
-    float tempVolts = tempReading * 5.0 / 1024.0;
-    float tempC = (tempVolts - 0.5) * 10.0;
-    float tempF = tempC * 9.0 / 5.0 + 32.0;
-  */
-  // Display Temperature in C
-//  lcd.setCursor(0, 0);
-//  lcd.print("Temp         C  ");
-//  // Display Temperature in F
-//  //lcd.print("Temp         F  ");
-//  lcd.setCursor(6, 0);
-//  // Display Temperature in C
-//  lcd.print(tempC);
-//  // Display Temperature in F
-//  //lcd.print(tempF);
-//  delay(500);
+  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
+  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
+  float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
 
+//   Display Temperature in C
   lcd.setCursor(0, 0);
-  lcd.print("Where is the");
-  lcd.setCursor(0, 1);
-  lcd.print("temp thing?");
-  // 10k
+//  lcd.print("Temp         C  ");
+  // Display Temperature in F
+  lcd.print("Temp         F ");
+  lcd.setCursor(6, 0);
+  // Display Temperature in C
+//  lcd.print(tempC);
+  // Display Temperature in F
+  lcd.print(tempF
+  );
+  delay(500);
+
+//  lcd.setCursor(0, 0);
+//  lcd.print("Where is the");
+//  lcd.setCursor(0, 1);
+//  lcd.print("temp thing?");
+//  10k
   }
